843b12daccd05e74cc642ddbcfa090e90e1a04b1
seeo <aragonlotr5@gmail.com>
2019-05-24 09:38:48 +0800

    able to retrieve an image of a puppy from Turbo360.

--- src/client/components/InstaClone.js
+++ src/client/components/InstaClone.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet, Image} from 'react-native';
 
 class InstaClone extends Component{
     render(){
         return(
             <View>
                 <Text>A wonderful clone of Instagram!</Text>
+                <Image
+                style = {{width: 50, height: 50}}
+                source = {{
+                            uri: "https://lh3.googleusercontent.com/FAr81UhFiTHfZokqKPnDOy1NSKa1bZNETqCPg9QEnF_1vkXSPTCRSXZUIUYaCfAQ_Z8ois6SD9eArsxAllSiOSiteQ"
+                        }
+                        //"https: //www.petmd.com/sites/default/files/petmd-puppy-weight.jpg"
+                        }
+                />
             </View>
         );
     }
