a7c49bb4986c15445fb82c0b98e797b0a1ca3326
seeo <aragonlotr5@gmail.com>
2019-05-26 23:16:21 +0800

    copy and pasted react-native-camera camera class component from the documentations.

--- src/client/components/screens/CameraScreen.js
+++ src/client/components/screens/CameraScreen.js
@@ -1,33 +1,72 @@
-import React, {Component} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { RNCamera } from 'react-native-camera';
 
-class CameraScreen extends Component{
-    login() {
-         /*navigate to switchNavigator, and then access the main key that gets mapped to MainFeed  */
-        this.props.navigation.navigate("main");
-        alert('pressed');
-    }
-    /*need to remmeber to put paratheses after the function if you want to call it.
-    But without if you just want to reference it */
+class CameraScreen extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <RNCamera
+          ref={ref => {
+            this.camera = ref;
+          }}
+          style={styles.preview}
+          type={RNCamera.Constants.Type.back}
+          flashMode={RNCamera.Constants.FlashMode.on}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use camera',
+            message: 'We need your permission to use your camera',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          androidRecordAudioPermissionOptions={{
+            title: 'Permission to use audio recording',
+            message: 'We need your permission to use your audio',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          onGoogleVisionBarcodesDetected={({ barcodes }) => {
+            console.log(barcodes);
+          }}
+        />
+        <View style={{ flex: 0, flexDirection: 'row', justifyContent: 'center' }}>
+          <TouchableOpacity onPress={this.takePicture.bind(this)} style={styles.capture}>
+            <Text style={{ fontSize: 14 }}> SNAP </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    );
+  }
 
-    render(){
-        return (
-            <TouchableOpacity
-                style ={{
-                    flex: 1,
-                    height: 100 +"%",
-                    width: 100+ "%",
-                    justifyContent: "center",
-                    alignItems: "center",
-                }}
-                onPress = {()=>{
-                    this.login()
-                }}
-            >
-                <Text>Camera PAGE</Text>
-            </TouchableOpacity>
-        );
+  takePicture = async function() {
+    if (this.camera) {
+      const options = { quality: 0.5, base64: true };
+      const data = await this.camera.takePictureAsync(options);
+      console.log(data);
     }
+  };
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    backgroundColor: 'black',
+  },
+  preview: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  capture: {
+    flex: 0,
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    padding: 15,
+    paddingHorizontal: 20,
+    alignSelf: 'center',
+    margin: 20,
+  },
+});
 
 export default CameraScreen;
